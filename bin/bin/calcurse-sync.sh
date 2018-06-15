8a2620bdb5e555f887d69ac7aa642b1b8b52107b
AxelBohm <axel.boehm@ymail.com>
2018-06-15 10:37:57 +0200

    added bin for userspecific scripts

---
+++ bin/bin/calcurse-sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
+calcurse-caldav --config ~/.calcurse/caldav/config_todo --syncdb ~/.calcurse/caldav/sync_todo.db

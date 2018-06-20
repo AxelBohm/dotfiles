c6b4da795cd2d9fa86df6b6990f2505837668f6f
AxelBohm <axel.boehm@ymail.com>
2018-06-20 22:53:51 +0200

    an install script for R packages from cl. needs littler

---
+++ bin/bin/install.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env r
+
+if (is.null(argv) | length(argv)<1) {
+  cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]\n")
+  q()
+}
+
+repos <- "http://cran.rstudio.com"
+
+# lib.loc <- "/usr/local/lib/R/site-library" # need to be run as root
+# install.packages(argv, lib.loc, repos)
+
+# install into whatever default location R chooses
+install.packages(argv, repos = repos)

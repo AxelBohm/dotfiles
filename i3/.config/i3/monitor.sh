236b426812208986ab9250983f32cf56f4080c46
AxelBohm <axel.boehm@ymail.com>
2018-08-24 09:42:41 +0200

    renamed variable

--- i3/.config/i3/monitor.sh
+++ i3/.config/i3/monitor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# one still has to run "chmod +x monitor.sh"
-EXTERNAL_OUTPUT="VGA-1"
-INTERNAL_OUTPUT="LVDS-1"
+EXTERNAL="VGA-1"
+INTERNAL="LVDS-1"
 
 if [ ! -f ~/.config/i3/monitor_mode.txt ] ; then
     monitor_mode="all"
@@ -11,16 +10,16 @@ fi
 
 if [ $monitor_mode = "all" ]; then
         monitor_mode="external"
-        xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
+        xrandr --output $INTERNAL --off --output $EXTERNAL --auto
 elif [ $monitor_mode = "external" ]; then
         monitor_mode="clones"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
+        xrandr --output $INTERNAL --auto --output $EXTERNAL --auto --same-as $INTERNAL
 elif [ $monitor_mode = "clones" ]; then
         monitor_mode="internal"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
+        xrandr --output $INTERNAL --auto --output $EXTERNAL --off
 else
         monitor_mode="all"
-        xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
+        xrandr --output $INTERNAL --auto --output $EXTERNAL --auto --left-of $INTERNAL
 fi
 
 # set wallpaper again

740330e3d075dd5126668000bfca77e3c1881e89
AxelBohm <axel.boehm@ymail.com>
2018-09-27 10:39:29 +0200

    transitioned to lpass-cli

--- bin/bin/calcurse-sync.sh
+++ bin/bin/calcurse-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
-calcurse-caldav --config ~/.calcurse/caldav/config_todo --syncdb ~/.calcurse/caldav/sync_todo.db
+CALCURSE_CALDAV_PASSWORD=$(lpass show --password fruux-arch) calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
+CALCURSE_CALDAV_PASSWORD=$(lpass show --password fruux-arch) calcurse-caldav --config ~/.calcurse/caldav/config_todo --syncdb ~/.calcurse/caldav/sync_todo.db

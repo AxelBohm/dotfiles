c995e7643e06d83c09be510116c15cc068d4ab28
AxelBohm <axel.boehm@ymail.com>
2019-05-23 10:08:20 -0400

    dunst notification for more feedback

--- bin/bin/link_grabber.sh
+++ bin/bin/link_grabber.sh
@@ -2,3 +2,4 @@
 regex='(((http|https|ftp|gopher))[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
 url=$(grep -Po "$regex" | uniq | dmenu -l 10 -p "Go:") || exit
 xdg-open "$url"
+notify-send "opening link in browser"

aa30f5c168f7cdf0e0d9b02bbccc0f593b09ae4c
AxelBohm <axel.boehm@ymail.com>
2019-05-26 19:19:40 -0400

    youtube-viewer not working

--- bin/bin/yt_grabber.sh
+++ bin/bin/yt_grabber.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 regex='(((https://www.youtube))[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | uniq | dmenu -l 10 -p "Go:") || exit
-youtube-viewer "$url"
+url=$(grep -Po "$regex" | uniq | dmenu -l 10 -p "watch:") || exit
+# youtube-viewer "$url"
+if [ -n "$url" ]; then
+    notify-send "loading youtube video"
+    mpv --ytdl  "$url"
+fi
+

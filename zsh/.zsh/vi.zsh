18b952baaf29340ed38dfa3199022f53c5d6188c
AxelBohm <axel.boehm@ymail.com>
2019-06-03 10:55:07 -0400

    zsh

---
+++ zsh/.zsh/vi.zsh
@@ -0,0 +1,33 @@
+###############################################################
+# => vi mode
+###############################################################
+
+# leave insert mode without esc
+bindkey -M viins 'jk' vi-cmd-mode
+
+# change curser depending on mode
+zle-keymap-select () {
+if [ $KEYMAP = vicmd ]; then
+    printf "\033[2 q"
+else
+    printf "\033[4 q"
+fi
+}
+zle -N zle-keymap-select
+zle-line-init () {
+zle -K viins
+printf "\033[4 q"
+}
+zle -N zle-line-init
+bindkey -v
+
+# rebind some of the keys
+bindkey "^R" history-incremental-search-backward
+bindkey "\eq" quote-line
+bindkey "^K" backward-kill-line
+
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+
+bindkey "^P" up-line-or-search
+bindkey "^N" down-line-or-search

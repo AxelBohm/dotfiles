87b2b62932bd4b4db2a69e3ce953ebf9e850ac43
AxelBohm <axel.boehm@ymail.com>
2019-06-04 09:51:44 -0400

    some emacs stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,8 @@
 
 ; startup screen
 (setq inhibit-startup-screen t)
+
+; for graphical emacs
 (menu-bar-mode -1) 
 (toggle-scroll-bar -1) 
 (tool-bar-mode -1) 
@@ -11,27 +13,49 @@
 ; stop creating #autosave# files
 (setq auto-save-default nil) 
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-  (package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+; ; List the packages you want
+; (setq package-list '(evil
+;                      org-bullets
+;                      evil-leader))
+; Activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+; ; Update your local package index
+; (unless package-archive-contents
+;   (package-refresh-contents))
+
+; ; Install all missing packages
+; (dolist (package package-list)
+;   (unless (package-installed-p package)
+;     (package-install package)))
 
 
+(use-package org-bullets
+:ensure t
+:init
+(setq org-bullets-bullet-list
+'("◉" "◎" "<img draggable="false" class="emoji" alt="⚫" src="https://s0.wp.com/wp-content/mu-plugins/wpcom-smileys/twemoji/2/svg/26ab.svg">" "○" "►" "◇"))
+:config
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(require 'evil)
+(use-package evil)
   (evil-mode 1)
+
+
+(use-package evil-leader)
+  (global-evil-leader-mode)
+  (evil-leader/set-leader " ")
+  (evil-leader/set-key
+    "b" 'switch-to-buffer
+    "w" 'save-buffer)

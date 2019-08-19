37638e7ecc4289a75e9d8d1c6a4cbe65d92f388e
AxelBohm <axel.boehm@ymail.com>
2019-08-18 22:04:05 -0500

    simplyfy + colors

--- bin/bin/fuzzy_pass.sh
+++ bin/bin/fuzzy_pass.sh
@@ -7,8 +7,10 @@ while getopts 'pu' flag; do
     esac
 done
 
-password=$(find ~/.password-store/ -name '*.gpg' |
-    cut -d'/' -f5- | sed 's/.\{4\}$//' | fzf)
+fzf_colours="--color=fg:7,fg+:4,pointer:4,prompt:3,header:2,hl:6,hl+:2,gutter:0"
+
+password=$(pass git ls-files '*.gpg' | sed 's/.gpg$//' |
+    fzf "${fzf_colours}" --reverse --margin=5%,25% --header="Select a password:")
 
 case $info in 
     (password) [ -n "$password" ] && pass show -c "$password" ;;

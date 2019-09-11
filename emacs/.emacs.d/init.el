8043584949ba79bbaed6d9ba8f1b7c908e319df4
AxelBohm <axel.boehm@ymail.com>
2019-09-11 11:39:35 -0500

    added snippets

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -36,7 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-level-1 ((t (:inherit outline-1 :height 1.9))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :foreground "#A3BE8C" :height 1.5))))
+ '(org-level-3 ((t (:inherit outline-3 :foreground "#81A1C1" :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :foreground "#8FBCBB" :height 1.0))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))

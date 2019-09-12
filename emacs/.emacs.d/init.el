67eda88a2f82d26139c54264fe18a99ad38e801d
AxelBohm <axel.boehm@ymail.com>
2019-09-12 17:22:00 -0500

    snippets

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,14 +29,14 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile dashboard page-break-lines auctex-latexmk which-key smartparens-config yasnippet use-package smartparens pretty-mode org-bullets nord-theme evil-leader evil-commentary company auctex))))
+    (latex-pretty-symbols projectile dashboard page-break-lines auctex-latexmk which-key smartparens-config yasnippet use-package smartparens pretty-mode org-bullets nord-theme evil-leader evil-commentary company auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-level-1 ((t (:inherit outline-1 :height 1.9))))
- '(org-level-2 ((t (:inherit outline-2 :foreground "#A3BE8C" :height 1.5))))
- '(org-level-3 ((t (:inherit outline-3 :foreground "#81A1C1" :height 1.2))))
- '(org-level-4 ((t (:inherit outline-4 :foreground "#8FBCBB" :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))

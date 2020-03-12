1b9435614b78d23413bef73ad7a9d957ab826bb5
AxelBohm <axel.boehm@ymail.com>
2020-03-12 18:14:53 +0100

    only print first line of password

--- bin/bin/calcurse-sync.sh
+++ bin/bin/calcurse-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CALCURSE_CALDAV_PASSWORD=$(pass show Business/fruux-arch) calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
+CALCURSE_CALDAV_PASSWORD=$(pass show Uni/univie-rechner | sed 1q) calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
 # CALCURSE_CALDAV_PASSWORD=$(pass show --password fruux-arch) calcurse-caldav --config ~/.calcurse/caldav/config_todo --syncdb ~/.calcurse/caldav/sync_todo.db

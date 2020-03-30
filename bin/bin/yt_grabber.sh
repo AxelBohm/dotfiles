daab6a7f585e3067d9317d9c80bfeb9c7f8af4d5
AxelBohm <axel.boehm@ymail.com>
2020-03-30 11:50:24 +0200

    if there's only one link no need for dmenu, just play

--- bin/bin/yt_grabber.sh
+++ bin/bin/yt_grabber.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
 regex='(((https://www.youtube))[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | uniq | dmenu -l 10 -p "watch:") || exit
+
+url=$(grep -Po "$regex" | uniq)
+
+if [ "$("$url" | wc -l)" -gt 1 ]
+then
+    url=$("$url" | dmenu -l 10 -p "watch:") || exit
+fi
+
+# url=$(grep -Po "$regex" | uniq | dmenu -l 10 -p "watch:") || exit
 # youtube-viewer "$url"
 if [ -n "$url" ]; then
     notify-send "loading youtube video"

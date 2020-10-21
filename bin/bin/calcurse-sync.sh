5d926eaf4194a8d93d960a7f4b43fa9aeebf54e4
AxelBohm <axel.boehm@ymail.com>
2020-10-21 16:43:36 +0200

    set path for cron

--- bin/bin/calcurse-sync.sh
+++ bin/bin/calcurse-sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 CALCURSE_CALDAV_PASSWORD=$(pass show Uni/univie-rechner | sed 1q) calcurse-caldav --config ~/.calcurse/caldav/config_cal --syncdb ~/.calcurse/caldav/sync_cal.db
 # CALCURSE_CALDAV_PASSWORD=$(pass show --password fruux-arch) calcurse-caldav --config ~/.calcurse/caldav/config_todo --syncdb ~/.calcurse/caldav/sync_todo.db

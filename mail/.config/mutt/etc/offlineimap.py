06925153a414dd9bdb7c8e6f03f454ad6c59b93f
AxelBohm <axel.boehm@ymail.com>
2021-02-02 16:56:30 +0100

    yahoo stopped working. added otp password for offlineimap

--- mail/.config/mutt/etc/offlineimap.py
+++ mail/.config/mutt/etc/offlineimap.py
@@ -4,3 +4,6 @@ from subprocess import check_output
 
 def get_pass(account):
     return check_output("pass Email/" + account, shell=True).splitlines()[0]
+
+def get_otp():
+    return check_output("pass show Email/yahoo.com | grep one-time-yahoo | awk '{print $2}'", shell=True).splitlines()[0]

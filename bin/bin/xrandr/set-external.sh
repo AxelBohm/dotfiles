c140e605dc08f43ea7e2c51204cbe88eb177e22f
AxelBohm <axel.boehm@ymail.com>
2022-12-12 10:14:40 +0000

    account for different internal monitor names in xrandr scripts

--- bin/bin/xrandr/set-external.sh
+++ bin/bin/xrandr/set-external.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-internal="LVDS1"
 
+# find name of internal monitor
 monitors=$(xrandr -q)
+declare -a possible_internal_names=("LVDS1" "eDP1")
+for int_port in "${possible_internal_names[@]}"
+do 
+    if echo "$monitors" | grep -q $int_port
+    then
+        internal=$int_port
+    fi
+done
+
+
+# somehow it doesn't work all the time?
 declare -a ports=("DP" "HDMI" "VGA")
 # loop over connectors and break if one of them is connected
 for port in "${ports[@]}"
 do
-    external=$(echo "$monitors" | grep "$port. con" | awk '{print $1}')
+    external=$(echo "$monitors" | grep "^$port. con" | awk '{print $1}')
     xrandr --output $internal --off --output "$external" --auto && break
 done
+
+
+# other possible solution
+# external_dp=$(xrandr | grep -o "^DP[\w'-]*\w* connected" | awk '{print$1}')
+# external_hdmi=$(xrandr | grep -o "^HDMI[\w'-]*\w* connected" | awk '{print$1}')
+# xrandr --output $internal --off --output "$external_dp" --auto --output "$externalhdmi" --auto
 
 
 # set wallpaper again
--- bin/bin/xrandr/set-internal.sh
+++ bin/bin/xrandr/set-internal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-internal=$(xrandr | grep -o "LVDS[\w'-]*\w*")
+
 externalvga=$(xrandr | grep -o "VGA[\w'-]*\w*")
 externalhdmi=$(xrandr | grep -o "HDMI[\w'-]*\w* connected")
 # remove the keyword "connected"
@@ -8,6 +8,18 @@ externalhdmi=${externalhdmi% connected}
 # remove the space at the end
 # externalhdmi="${externalhdmi::-1}"
 # echo "$externalhdmi"
+
+
+# find name of internal monitor
+monitors=$(xrandr -q)
+declare -a possible_internal_names=("LVDS1" "eDP1")
+for int_port in "${possible_internal_names[@]}"
+do
+    if echo "$monitors" | grep -q $int_port
+    then
+        internal=$int_port
+    fi
+done
 
 
 xrandr --output $internal --auto --output $externalvga --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output DP2 --off
